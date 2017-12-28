7b08a3ac05cad08c339af00d25a329f0411a8d90
eioannidis <eioannidis@cite.gr>
2017-12-28 17:36:04 +0200

    Created and started with the login project.

---
+++ LoginApp5/app/model.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Model = /** @class */ (function () {
+    function Model(id, firstName, lastName, userName, password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+    }
+    return Model;
+}());
+exports.Model = Model;
+//# sourceMappingURL=model.js.map
---
+++ LoginApp5/app/model.ts
@@ -0,0 +1,9 @@
+﻿export class Model {
+    constructor(
+        public id: number ,
+        public firstName: string ,
+        public lastName: string,
+        public userName: string,
+        public password: string
+    ) { }
+}
